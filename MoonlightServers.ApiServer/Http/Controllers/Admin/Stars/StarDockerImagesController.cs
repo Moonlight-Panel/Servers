c469814782b9207e8214f853ed146f21d775b0b8
ChiaraBm <chiara.bm04@gmail.com>
2024-12-06 15:41:05 +0100

    Added docker image options to star crud

---
+++ MoonlightServers.ApiServer/Http/Controllers/Admin/Stars/StarDockerImagesController.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using MoonCore.Exceptions;
+using MoonCore.Extended.Abstractions;
+using MoonCore.Extended.Helpers;
+using MoonCore.Helpers;
+using MoonCore.Models;
+using MoonlightServers.ApiServer.Database.Entities;
+using MoonlightServers.Shared.Http.Requests.Admin.StarDockerImages;
+using MoonlightServers.Shared.Http.Responses.Admin.StarDockerImages;
+
+namespace MoonlightServers.ApiServer.Http.Controllers.Admin.Stars;
+
+[ApiController]
+[Route("api/admin/servers/stars")]
+public class StarDockerImagesController : Controller
+{
+    private readonly CrudHelper<StarDockerImage, StarDockerImageDetailResponse> CrudHelper;
+    private readonly DatabaseRepository<Star> StarRepository;
+    private readonly DatabaseRepository<StarDockerImage> StarDockerImageRepository;
+    
+    private Star Star;
+
+    public StarDockerImagesController(
+        CrudHelper<StarDockerImage, StarDockerImageDetailResponse> crudHelper,
+        DatabaseRepository<Star> starRepository,
+        DatabaseRepository<StarDockerImage> starDockerImageRepository
+    )
+    {
+        CrudHelper = crudHelper;
+        StarRepository = starRepository;
+        StarDockerImageRepository = starDockerImageRepository;
+    }
+    
+    private void ApplyStar(int id)
+    {
+        var star = StarRepository
+            .Get()
+            .FirstOrDefault(x => x.Id == id);
+
+        if (star == null)
+            throw new HttpApiException("A star with this id could not be found", 404);
+
+        Star = star;
+
+        CrudHelper.QueryModifier = dockerImages =>
+            dockerImages.Where(x => x.Star.Id == star.Id);
+    }
+
+    [HttpGet("{starId:int}/dockerImages")]
+    public async Task<IPagedData<StarDockerImageDetailResponse>> Get([FromRoute] int starId, [FromQuery] int page, [FromQuery] int pageSize)
+    {
+        ApplyStar(starId);
+        
+        return await CrudHelper.Get(page, pageSize);
+    }
+
+    [HttpGet("{starId:int}/dockerImages/{id:int}")]
+    public async Task<StarDockerImageDetailResponse> GetSingle([FromRoute] int starId, [FromRoute] int id)
+    {
+        ApplyStar(starId);
+        
+        return await CrudHelper.GetSingle(id);
+    }
+
+    [HttpPost("{starId:int}/dockerImages")]
+    public async Task<StarDockerImageDetailResponse> Create([FromRoute] int starId, [FromBody] CreateStarDockerImageRequest request)
+    {
+        ApplyStar(starId);
+        
+        var starDockerImage = Mapper.Map<StarDockerImage>(request);
+        starDockerImage.Star = Star;
+
+        var finalVariable = StarDockerImageRepository.Add(starDockerImage);
+
+        return CrudHelper.MapToResult(finalVariable);
+    }
+
+    [HttpPatch("{starId:int}/dockerImages/{id:int}")]
+    public async Task<StarDockerImageDetailResponse> Update([FromRoute] int starId, [FromRoute] int id,
+        [FromBody] UpdateStarDockerImageRequest request)
+    {
+        ApplyStar(starId);
+        
+        return await CrudHelper.Update(id, request);
+    }
+
+    [HttpDelete("{starId:int}/dockerImages/{id:int}")]
+    public async Task Delete([FromRoute] int starId, [FromRoute] int id)
+    {
+        ApplyStar(starId);
+        
+        await CrudHelper.Delete(id);
+    }
+}
---
+++ MoonlightServers.Shared/Http/Requests/Admin/StarDockerImages/CreateStarDockerImageRequest.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MoonlightServers.Shared.Http.Requests.Admin.StarDockerImages;
+
+public class CreateStarDockerImageRequest
+{
+    [Required(ErrorMessage = "You need to provide a display name")]
+    public string DisplayName { get; set; }
+    
+    [Required(ErrorMessage = "You need to specify a docker image identifier")]
+    [RegularExpression("^(?:(?=[^:\\/]{1,253})(?!-)[a-zA-Z0-9-]{1,63}(?<!-)(?:\\.(?!-)[a-zA-Z0-9-]{1,63}(?<!-))*(?::[0-9]{1,5})?\\/)?((?![._-])(?:[a-z0-9._-]*)(?<![._-])(?:\\/(?![._-])[a-z0-9._-]*(?<![._-]))*)(?::(?![.-])[a-zA-Z0-9_.-]{1,128})?$", ErrorMessage = "You need to specify a valid docker image identifier")]
+    public string Identifier { get; set; }
+    
+    public bool AutoPulling { get; set; }
+}
