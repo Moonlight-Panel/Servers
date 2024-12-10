ab322d4dedad7f608d143182762fd754aa990700
ChiaraBm <chiara.bm04@gmail.com>
2024-12-10 22:31:28 +0100

    Updated css loading to use the new bundler

--- MoonlightServers.ApiServer/Startup/PluginStartup.cs
+++ MoonlightServers.ApiServer/Startup/PluginStartup.cs
@@ -1,21 +1,26 @@
 using MoonCore.Extensions;
 using Moonlight.ApiServer.Interfaces.Startup;
+using Moonlight.ApiServer.Services;
 
 namespace MoonlightServers.ApiServer.Startup;
 
 public class PluginStartup : IAppStartup
 {
     private readonly ILogger<PluginStartup> Logger;
+    private readonly BundleService BundleService;
 
-    public PluginStartup(ILogger<PluginStartup> logger)
+    public PluginStartup(ILogger<PluginStartup> logger, BundleService bundleService)
     {
         Logger = logger;
+        BundleService = bundleService;
     }
 
     public Task BuildApp(IHostApplicationBuilder builder)
     {
         // Scan the current plugin assembly for di services
         builder.Services.AutoAddServices<PluginStartup>();
+        
+        BundleService.BundleCss("css/MoonlightServers.min.css");
 
         return Task.CompletedTask;
     }
